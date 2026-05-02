d6dc18f8ae54a7800b0599046e95371252ab1c8a
Dakota Buckley <statisticsman1@gmail.com>
2026-05-02 09:07:58 -0600

    Add lock

--- subscriptions/dakotabuilds/main.tf
+++ subscriptions/dakotabuilds/main.tf
@@ -1,4 +1,10 @@
 resource "local_file" "test" {
     filename = "C:\\Users\\stati\\Desktop\\repos\\hi.txt"
-    content = "sup"
+    content = "Whats up ${random_pet.my-pet.id}"
+}
+
+resource "random_pet" "my-pet" {
+    length = 2
+    separator = "."
+    prefix = "mr"
 }
