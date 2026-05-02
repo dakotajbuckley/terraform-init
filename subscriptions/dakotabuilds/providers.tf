4967292b7154fe803785d86d4840b226a8849480
Dakota Buckley <statisticsman1@gmail.com>
2026-05-02 10:25:14 -0600

    Add backend

--- subscriptions/dakotabuilds/providers.tf
+++ subscriptions/dakotabuilds/providers.tf
@@ -4,4 +4,13 @@ terraform {
         source = "hashicorp/azurerm"
       }
     }
+
+    backend "azurerm" {
+      tenant_id = "6afe2c08-005f-4f3b-8ac3-99458dd7a365"
+      resource_group_name = "dakotabuilds-rg"
+      storage_account_name = "dakotabuilds"
+      container_name = "dakotabuilds-terraform-state"
+      key = "dakotabuilds-terraform-state"
+    }
 }
+
