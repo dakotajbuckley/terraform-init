7329f5dcfdf00e6be34c96ca21affe90e1e7380d
Dakota Buckley <statisticsman1@gmail.com>
2026-05-02 10:36:26 -0600

    Add dns zone

--- subscriptions/dakotabuilds/main.tf
+++ subscriptions/dakotabuilds/main.tf
@@ -22,3 +22,8 @@ resource "azurerm_storage_container" "dakotabuild-terraform-state" {
     storage_account_id = azurerm_storage_account.dakotabuilds.id
     container_access_type = "private"
 }
+
+resource "azurerm_dns_zone" "dakotabuilds-dns-zone" {
+    name = "dakotabuilds.dev"
+    resource_group_name = azurerm_resource_group.dakotabuilds-rg.name
+}
---
+++ subscriptions/dakotabuilds/moved.tf
@@ -0,0 +1,4 @@
+moved {
+  from = azurerm_dns_zone.dakotauilds-dns-zone
+  to = azurerm_dns_zone.dakotabuilds-dns-zone
+}
