8196a4ecc8d0fa14ceb88cf01b419157dc270e5c
Dakota Buckley <statisticsman1@gmail.com>
2026-05-03 07:45:13 -0600

    Add network module

---
+++ modules/azure_virtual_network/main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_virtual_network" "this" {
+    name = var.vnet_name
+    location = var.location
+    resource_group_name = var.resource_group_name
+    address_space = var.vnet_address_space
+}
+
+resource "azurerm_subnet" "this" {
+    for_each = var.subnets
+    name = var.subnets[each.key]
+    resource_group_name = var.resource_group_name
+    virtual_network_name = azurerm_virtual_network.this.name
+    address_prefixes = var.subnets[each.value]
+}
---
+++ modules/azure_virtual_network/variables.tf
@@ -0,0 +1,24 @@
+variable "vnet_name" {
+    description = "The name of the vnet that will be created"
+    type = string
+}
+
+variable "location" {
+    description = "The region that the vnet and subnets will be created in"
+    type = string
+}
+
+variable "resource_group_name" {
+    description = "The name of the resource group that will house the vnet"
+    type = string
+}
+
+variable "vnet_address_space" {
+    description = "The ip range for the vnet"
+    type = list(string)
+}
+
+variable "subnets" {
+    description = "The name of the subnets you'd like created"
+    type = map(string)
+}
